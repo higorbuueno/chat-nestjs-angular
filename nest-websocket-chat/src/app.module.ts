ec3dd4d2574ce9da40dca09f9601055eae015e89
Higor Bueno <buenoshigor@gmail.com>
2023-11-06 23:38:41 -0300

    Finalizado .envs de 'producao' e desenvolvimento

--- nest-websocket-chat/src/app.module.ts
+++ nest-websocket-chat/src/app.module.ts
@@ -4,24 +4,44 @@ import { AppService } from './app.service';
 import { ChatModule } from './chat/chat.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ChatEvent } from './chat/entities/chat-event.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { config } from 'dotenv';
+import * as path from 'path';
+
+if (process.env.NODE_ENV === 'production') {
+
+  // package.json:
+  // 
+  // "scripts": {
+  //   "start:prod": "NODE_ENV=production nest start --env production"
+  // }
+
+  config({ path: path.resolve(__dirname, '../.env.production') });
+} else {
+  config();
+}
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
     ChatModule,
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'mysql',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'chat',
-      entities: [ChatEvent],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>("DB_HOST"),
+        port: configService.get<number>("DB_PORT"),
+        username: configService.get<string>("DB_USERNAME"),
+        password: configService.get<string>("DB_PASSWORD"),
+        database: configService.get<string>("DB_NAME"),
+        entities: [ChatEvent],
+        synchronize: configService.get<boolean>("DB_SYNCHRONIZATION"),
+      })
     }),
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {
-  // constructor(private dataSource: DataSource) { }
-}
+export class AppModule { }
--- nest-websocket-chat/src/main.ts
+++ nest-websocket-chat/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>("APP_PORT")
+  await app.listen(port);
 }
 bootstrap();
